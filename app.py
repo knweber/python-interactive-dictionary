841ac1a9ff4f15432879413c0f9b80b5f4a01ded
Kris <kris.weber.1994@gmail.com>
2018-04-30 16:36:50 -0700

    Implement word suggestions

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #/usr/bin/python3
 import json 
-import difflib
-from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -10,7 +9,17 @@ def translate(w):
     if w in data:
         return data[w]
     else:
-        return "Uh oh! I don't think that word exists!"
+        if len(get_close_matches(w, data.keys())) > 0:
+            closest_match = get_close_matches(w, data.keys())[0]
+            yes_or_no = input("Did you mean " + closest_match + " instead? Enter Y if yes, or N if no.")
+            if yes_or_no == "Y":
+                return data[closest_match]
+            elif yes_or_no == "N":
+                return "Uh oh! I don't think that word exists!"
+            else:
+                return "Whoops! I didn't understand your answer."
+        else: 
+            return "Uh oh! I don't think that word exists!"
 
 word = input("Enter word: ")
 
