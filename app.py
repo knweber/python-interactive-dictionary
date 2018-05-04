10cb5bc792370fe48517d225c30151a29030e7e2
Kris <kris.weber.1994@gmail.com>
2018-05-04 09:56:55 -0700

    Fix bugs to find definitions for acronyms, proper nouns, and multi-word proper nouns

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
+def no_match():
+  return "Uh oh! I don't think that word exists!"
+
+def error():
+  return "Whoops! I didn't understand your answer."
+
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     else:
         if len(get_close_matches(w, data.keys())) > 0:
             closest_match = get_close_matches(w, data.keys())[0]
@@ -15,13 +25,19 @@ def translate(w):
             if yes_or_no == "Y":
                 return data[closest_match]
             elif yes_or_no == "N":
-                return "Uh oh! I don't think that word exists!"
+                return no_match()
             else:
-                return "Whoops! I didn't understand your answer."
+                return error()
         else: 
-            return "Uh oh! I don't think that word exists!"
+            return no_match()
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for definition in output:
+        print(definition)
+else:
+    print(output)
 
